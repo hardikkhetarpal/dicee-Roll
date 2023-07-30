54317b474ebf6d55c2447f9143ce3c40db8ba081
hardik <hardikkhetarpal2@gmail.com>
2023-07-30 14:14:05 -0400

    first commit

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -4,12 +4,14 @@ var randomNumber2=Math.ceil(Math.random()*6);
 document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
 document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
 
+
+
 if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="player 1 wins";
+    document.querySelector("p").innerHTML="<b>Player 1 wins</b> Refresh me to Roll again";
 }
 else if(randomNumber1===randomNumber2){
-    document.querySelector("h1").innerHTML="Draw";
+    document.querySelector("p").innerHTML="<b>Draw</b> Refresh me to Roll again";
 }
 else{
-    document.querySelector("h1").innerHTML="player 2 wins";
+    document.querySelector("p").innerHTML="<b>player 2 wins</b> Refresh me to Roll again";
 }
